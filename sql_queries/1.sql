a9b9368497572a85b1483fbe86a5f8643e3e5428
Ifiokobong Akpan <iakpan916@gmail.com>
2022-02-13 21:37:12 +0100

    Updated the queries

--- sql_queries/1.sql
+++ sql_queries/1.sql
@@ -59,3 +59,7 @@ where University = 'University of London' and Course_Rating > 4.7
 -- Courses whose course rating is NULL
 select Course_Name, [Course _URL], Course_Rating from CourseraCourses..Coursera
 where Course_Rating is null
+
+-- Number of courses that have course rting greater than 4.5
+select COUNT(*) from CourseraCourses..Coursera
+where Course_Rating > 4.5
